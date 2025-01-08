17ead3b9a9c05f167632ee9a91f4786be090ed00
“Ibrahim” <bojackmanhorse1234@gmail.com>
2025-01-08 12:19:10 +0000

    Second commit

--- Gaussian/Basic.lean
+++ Gaussian/Basic.lean
@@ -3,7 +3,7 @@ import ZKLib.Data.MlPoly
 import ZKLib.Data.MvPolynomial
 import Mathlib.Algebra.Module.Defs
 
-namespace test
+namespace linear_equations
 
 /-
 
@@ -28,13 +28,34 @@ structure linear_equation (len : ℕ) (α : Type*) [CommRing α] [Inhabited α] 
   coefficients : Array α
   length : coefficients.size = len
 
-/- Here's the addition operation on `linear_equation`: -/
-instance add_linear_equations {len : ℕ} {α : Type*} [CommRing α] [Inhabited α] : Add (linear_equation len α) where
-  add p q := ⟨Array.map (fun x => x.1 + x.2) (Array.zip p.coefficients q.coefficients), by rw [Array.size_map, Array.size_zip, p.length, q.length]; simp⟩
+lemma add_zip_array_size {len : ℕ} {α : Type*} [CommRing α] [Inhabited α] (p q : linear_equation len α): (Array.map (fun x => x.1 + x.2) (Array.zip p.coefficients q.coefficients)).size = len := by
+  rw [Array.size_map, Array.size_zip, p.length, q.length]
+  simp
 
-def add_assoc_lin_eqn {len : ℕ} {α : Type*} [CommRing α] [Inhabited α] (p q r : linear_equation len α) : p + q + r = p + (q + r) := by
+
+/- Here's the addition operation on `linear_equation`: -/
+instance {len : ℕ} {α : Type*} [CommRing α] [Inhabited α] : Add (linear_equation len α) where
+  add p q := ⟨Array.map (fun x => x.1 + x.2) (Array.zip p.coefficients q.coefficients), by apply add_zip_array_size⟩
+
+@[simp]
+lemma defn_of_add {len : ℕ} {α : Type*} [CommRing α] [Inhabited α] (p q : linear_equation len α) (i : ℕ) (h : i < len) : (p + q).coefficients[i]'(by rw [(p + q).length]; assumption ) = (Array.map (fun x => x.1 + x.2) (Array.zip p.coefficients q.coefficients))[i]'(by rw [add_zip_array_size]; assumption) := by
+  rfl
+
+
+lemma index_is_linear {len : ℕ} {α : Type*} [CommRing α] [Inhabited α] [Add (linear_equation len α)]
+  (p q : linear_equation len α) (i : ℕ) (h : i < len) :
+  (p + q).coefficients[i]'(by rw [(p + q).length]; assumption ) = p.coefficients[i]'(by rw [p.length]; assumption ) + q.coefficients[i]'(by rw [q.length]; assumption ) := by
+  simp
+  sorry
+
+theorem add_assoc_lin_eqn {len : ℕ} {α : Type*} [CommRing α] [Inhabited α] (p q r : linear_equation len α) : p + q + r = p + (q + r) := by
   ext
-  .
+  . rw [(p + q + r).length, (p + (q + r)).length]
+  . sorry
+
+lemma array_add_size {α : Type*} [CommRing α] [Inhabited α] {len : ℕ} (p q : Array α) (h₁ : p.size = len) (h₂ : q.size = len) : (Array.map (fun x => x.1 + x.2) (Array.zip p q)).size = len := by
+  rw [Array.size_map, Array.size_zip, h₁, h₂]
+  simp
 
 def eval_poly {α : Type*} [CommRing α] [Inhabited α] {len : ℕ} (poly : linear_equation len α) (pts : Array α) : α :=
   (∑ i < len - 1, pts[i]! * poly.coefficients[i]!) + poly.coefficients[len - 1]!
@@ -44,6 +65,4 @@ def eval_poly {α : Type*} [CommRing α] [Inhabited α] {len : ℕ} (poly : line
 instance linear_equation_add_monoid (len : ℕ) (α : Type*) [CommRing α] [Inhabited α] : AddCommGroup (linear_equation len α) where
   add p q := ⟨ Array.map (fun x => x.1 + x.2) (Array.zip p.coefficients q.coefficients), by rw [Array.size_map, Array.size_zip, p.length, q.length]; simp⟩
 
-
-
-end test
+end linear_equations
