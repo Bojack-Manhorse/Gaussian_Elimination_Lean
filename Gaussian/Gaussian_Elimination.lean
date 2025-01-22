f6720df9947af1dfe8c12bacf8d9ddca6dbc81df
“Ibrahim” <bojackmanhorse1234@gmail.com>
2025-01-22 11:19:34 +0000

    Writing algorithm to read solution when there's only a unique one.

--- Gaussian/Gaussian_Elimination.lean
+++ Gaussian/Gaussian_Elimination.lean
@@ -51,17 +51,20 @@ For each l : Check system[l][l] ≠ 0
 Have a representation thats of the form Fin num_rows → Fun num_columns → α
 
 -/
+section unnamed
 
-variable {α : Type} [Field α] [DecidableEq α] [Inhabited α] {k n : {x : ℕ // x > 1}} (i j : ℕ) (h₁ : i < n) (h₂ : j < k) (kgtn : k > n)
+variable {α : Type} [Field α] [DecidableEq α] [Inhabited α] {k n : {x : ℕ // x > 1}} (kgtn : k > n) -- (i j : ℕ) (h₁ : i < n) (h₂ : j < k)
 
 def iltk (i : ℕ) (h : i < n.1) : i < k := Nat.lt_trans h kgtn
 
-
 /- Assuming `system[i][i] ≠ 0` makes `system[j][i] = 0`: -/
-def zero_entry (system : linearSystem α k n) (i j : ℕ) (h₀ : i < n) (h₁ : j < k) : linearSystem α k n := by
-    let coef := -((system[j]'(h₁))[i]'(h₀) / (system[i]'(Nat.lt_trans h₀ kgtn))[i])
-    exact add_row system coef i j (Nat.lt_trans h₀ kgtn) h₁
 
+def zero_entry (system : linearSystem α k n) (i j : ℕ ) (h₀ : i < n) (h₁ : j < k) : linearSystem α k n :=
+    let coef := -(system[j][i] / (system[i]'(Nat.lt_trans h₀ kgtn))[i])
+    add_row system coef i j (Nat.lt_trans h₀ kgtn) h₁
+
+#check zero_entry
+#check @zero_entry Nat
 
 lemma zero_entry_after_zero_entry (system : linearSystem α k n) (i j : ℕ) (h₀ : i < ↑n) (h₁ : j < ↑k) (h₃ : (system[i]'(Nat.lt_trans h₀ kgtn))[i]'(h₀) ≠ 0) : ((zero_entry kgtn system i j h₀ h₁)[j]'(h₁))[i]'(h₀) = 0 := by
     simp [zero_entry]
@@ -85,44 +88,75 @@ def zero_entry_pair (i j : ℕ) (h₀ : i < n) (h₁ : j < k) (h₂ : i ≠ j)
         (system_pair : system_non_zero_i_pair (α := α) kgtn i h₀) : (system_non_zero_i_pair (α := α) kgtn i h₀) :=
     ⟨zero_entry kgtn system_pair.system i j h₀ h₁, i_th_index_stays_non_zero kgtn system_pair.system i j h₀ h₁ h₂ system_pair.non_zero_i⟩
 
-def pivot_system_above (system : linearSystem α k n) (i : ℕ) (h₁ : i < n) : (linearSystem α k n) := by
-    have h₉ : i < k := Nat.lt_trans h₁ kgtn
-    exact Fin.foldr i (fun (x : Fin i) (system' : (linearSystem α k n)) => zero_entry kgtn system' i x h₁ (Nat.lt_trans x.2 h₉) ) system
+/- The function we fold over in `pivot_system_above`: -/
+def fold_func_i (i : ℕ) (h₁ : i < n) := fun (system : (linearSystem α k n)) (x : Fin i) => zero_entry kgtn system i x h₁ (Nat.lt_trans x.2 (Nat.lt_trans h₁ kgtn))
+
+def pivot_system_above (system : linearSystem α k n) (i : ℕ) (h₁ : i < n) : (linearSystem α k n) :=
+    Fin.foldl i (fold_func_i kgtn i h₁) system
+
+lemma fold_l_swap' {β : Type} (n : ℕ) (f : β → ℕ → β) (b : β) : Fin.foldl (n + 1) (fun x y => f x y) b = f (Fin.foldl n (fun x y => f x y ) b) n := by
+    induction' n with i ih
+    .   rfl
+    .   rw [Fin.foldl, Fin.foldl.loop]
+        simp
+        rw [ih]
+        sorry
+
 
 lemma zero_above_pivot_system_above (system : linearSystem α k n) (i : ℕ) (h₁ : i < n) : (system[i]'(Nat.lt_trans h₁ kgtn))[i] ≠ 0 → ∀ eqn : Fin i, ((pivot_system_above kgtn system i h₁)[eqn]'(Nat.lt_trans (Nat.lt_trans eqn.2 h₁) kgtn))[i]'(h₁) = 0 := by
     intro sysneq0
     intro eqn
     rw [pivot_system_above]
-    /- How to induct on this?! -/
+    /-Use generalizing `system'` maybe?-/
+    induction' i  with i ih --generalizing system
+    .   rcases eqn; omega
+    .   rw [Fin.foldl_succ_last]
+        sorry
 
-
-def pivot_system_below (system : linearSystem α k n) (i : ℕ) (h₁ : i < n) : (linearSystem α k n) := by
+def pivot_system_below (system : linearSystem α k n) (i : ℕ) (h₁ : i < n) : (linearSystem α k n) :=
     have h₉ : i < k := Nat.lt_trans h₁ kgtn
     have h₈ (x : Fin (k - i - 1)) : x.1 + ↑i + 1 < ↑k := by
         rw [add_assoc]
         apply Nat.add_lt_of_lt_sub
         exact x.isLt
-    exact Fin.foldr (k - i - 1) (fun (x : Fin (k - i - 1)) (system' : (linearSystem α k n)) => zero_entry kgtn system' i (x + i + 1) h₁ (h₈ x) ) system
+    Fin.foldr (k - i - 1) (fun (x : Fin (k - i - 1)) (system' : (linearSystem α k n)) => zero_entry kgtn system' i (x + i + 1) h₁ (h₈ x) ) system
 
 lemma zero_below_pivot_system_below (system : linearSystem α k n) (i : ℕ) (h₁ : i < n)
         : (system[i]'(Nat.lt_trans h₁ kgtn))[i] ≠ 0 → ∀ eqn : Fin (k - i - 1), ((pivot_system_below kgtn system i h₁)[eqn + i - 1])[i]'(h₁) = 0 := sorry
 
+lemma array_to_list_membership {β : Type} (as : Array β) (x : β) : x ∈ as → x ∈ as.toList := by
+    exact fun a =>
+    a.val
+
+lemma array_range_inequal {x m : ℕ} (h: x ∈ Array.range m ) :  x < m := by
+    have h₂ : x ∈ (Array.range m).toList := array_to_list_membership (Array.range m) x h
+    have h₃ : x ∈ List.range m := by rw [Array.toList_range] at h₂; exact h₂
+    apply List.mem_range.mp
+    exact h₃
+
+def index_array_with_proof (m : ℕ) : List (Σ' x : ℕ, x < m) :=
+    (List.range m).attach.map (fun x : {x : ℕ // x ∈ List.range m} => ⟨x.1, List.mem_range.mp x.2⟩)
+
+def index_array_with_proof' (m : ℕ) : Array (Fin m) := Array.finRange m
+
+
 /- Makes the entry at `system[i][i]` non-zero -/
 def ensure_non_zero_at_i (system : linearSystem α k n) (i : ℕ) (h₁ : i < n) : linearSystem α k n :=
     /- Only look below the `i-th` column when looking for non-zero element -/
+    /- Add row and column function.-/
     let entire_i_th_column : Array α := system.toArray.map (fun lin_eqn => lin_eqn[i]'(h₁))
-    let entire_i_th_column_zipped : _ := entire_i_th_column.zipWithIndex
-    let i_th_column_below_zipped : Array (α × ℕ) := entire_i_th_column_zipped.extract i (k + 1)
+
+    let entire_i_th_column_zipped : _ := entire_i_th_column.zip (Array.finRange k)
+
+    let i_th_column_below_zipped : _ := entire_i_th_column_zipped.extract i (k + 1)
 
     let non_zero := Array.filter (fun x => x.1 != 0) i_th_column_below_zipped
-    if non_zero.size > 0 then
-        /- Question: How to pick first non-zero element of `i_th_column_array`?-/
-        let index : ℕ := (non_zero[0]!).2
-        have indltk : index < ↑k := sorry
-        by refine swap_row system i index (Nat.lt_trans h₁ kgtn) indltk
+    if h : non_zero.size > 0 then
+        let index : ℕ := (non_zero[0]'(h)).2
+        have indltk : index < ↑k := (non_zero[0]'(h)).2.2
+        swap_row system i index (Nat.lt_trans h₁ kgtn) indltk
     else
-        by exact system
-
+        system
 
 def pivot_system (system : linearSystem α k n) (i : ℕ) (h₁ : i < n) : (linearSystem α k n) := by
     apply pivot_system_above kgtn _ i h₁
@@ -142,6 +176,16 @@ lemma upper_triangular_after_row_reduce (system : linearSystem α k n)
 
 def system_in_ref (system : linearSystem α k n) : Prop := (system_in_upper_triangular kgtn system) ∧ (∀ i : Fin n, (system[i]'(Nat.lt_trans i.2 kgtn))[i] ≠ 0 → ∀ j : Fin k, system[j][i] = 0)
 
+def system_in_reduced_ref (system : linearSystem α k n) : Prop :=
+    ∀ i : Fin (n - 1), (system[i]'(Nat.lt_trans (fin_less_than n i) kgtn))[i] ≠ 0 ∧
+    ∀ (i : Fin (n - 1)) (j : Fin k), (i.1 ≠ j.1) → system[j][i] = 0
+
+def system_has_unique_solution (system : linearSystem α k n) : Prop :=
+    (∃ β : Vector α (n - 1), beta_is_solution system β ∧ ∀ γ : Vector α (n - 1), beta_is_solution system γ → γ = β)
+
+lemma unique_sol_implies_reduced_ref (system : linearSystem α k n) (unique_sol : system_has_unique_solution system) (rref : system_in_ref kgtn system)
+        : (system_in_reduced_ref kgtn system) := by sorry
+
 /- Need this to be a sigma type of ⟨rank, rank < n, ∀ x > rank, system[x] = 0 ⟩ -/
 def get_rank (system : linearSystem α k n) (h : system_in_upper_triangular kgtn system) : {x : ℕ // x < n} :=
     sorry
@@ -152,12 +196,21 @@ lemma non_zero_at_rank (system : linearSystem α k n) (h : system_in_upper_trian
 /- Last row has atleast two non-zero entries, if last entry is non-zero-/
 lemma last_row_proper_form (system : linearSystem α k n) (h : system_in_upper_triangular kgtn system) (has_sol : ∃ β, beta_is_solution system β) :
         let rank := get_rank kgtn system h
-        (system[rank.1]'(Nat.lt_trans rank.2 kgtn))[↑n - 1] = 0 → ∃ index : Fin (n - 1), (system[rank.1]'(Nat.lt_trans rank.2 kgtn))[index] ≠ 0 := sorry
+        ∃ index : Fin (n - 1), (system[rank.1]'(Nat.lt_trans rank.2 kgtn))[index] ≠ 0 := sorry
 
 
 
 /- Read of solution -/
 def get_solution (system : linearSystem α k n) (h : system_in_ref kgtn system) (has_sol : ∃ β, beta_is_solution system β) : Vector α (n - 1) := by
+    let rank := get_rank kgtn system h.1
+    let last_row := system[rank.1]'(Nat.lt_trans rank.2 kgtn)
+    have : last_row ≠ 0 := sorry
+    sorry
+
+def get_unique_solution (system : linearSystem α k n) (h : system_has_unique_solution system)
+        : Vector α (n - 1) :=
+
+
 
     /-let rank := get_rank kgtn system h
     let prop : Prop := (system[rank.1]'(Nat.lt_trans rank.2 kgtn))[rank.1] = (system[rank.1]'(Nat.lt_trans rank.2 kgtn))[rank.1 - 1]
@@ -166,7 +219,6 @@ def get_solution (system : linearSystem α k n) (h : system_in_ref kgtn system) 
     done-/
 
 
-
-
+end unnamed
 
 end Gaussian
